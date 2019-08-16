e58bbca619df4a5d4fe21e0f23edb92ea90e5c76
Chris Nyberg <chrisn83@gmail.com>
2019-08-16 11:02:14 -0400

    minor changes to footer, mapActionstoProps

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// import thunk from 'redux-thunk';
 import { updateUser } from './actions/usersActions';
+import { bindActionCreators } from 'redux';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
@@ -8,12 +10,9 @@ import NavbarPage from './components/NavBar/navbar';
 import LandingPage from './pages/LandingPage/landingPage';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
 
-  onUpdateUser = () => {
-    this.props.onUpdateUser('sammy');
+  onUpdateUser = (event) => {
+    this.props.onUpdateUser(event.target.value);
   }
   render() {
 
@@ -22,19 +21,32 @@ class App extends Component {
       <>
         <NavbarPage />
         <LandingPage />
-        {/* <input {} /> */}
-        <div onClick={this.onUpdateUser}>UPDATE</div>
+        <input onChange={this.onUpdateUser} />
         {this.props.user}
       </>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  products: state.products,
-  user: state.user
-});
-const mapActionsToProps = {
-  onUpdateUser: updateUser
+const mapStateToProps = (state, props) => {
+  console.log(props);
+
+  return {
+    products: state.products,
+    user: state.user,
+    userPlusProps: `${state.user} ${props.aRandomProps}`
+  };
+};
+const mapActionsToProps = (dispatch, props) => {
+  console.log(props)
+  return bindActionCreators({
+    onUpdateUser: updateUser
+  }, dispatch)
+};
+
+const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
+  console.log(propsFromState, propsFromDispatch, ownProps)
+  return {};
 }
+
 export default connect(mapStateToProps, mapActionsToProps)(App);
